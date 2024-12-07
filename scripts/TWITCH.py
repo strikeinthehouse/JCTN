463454afab72279c11ca0bc52ad29d60f1d72bc6
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-12-07 02:51:03 -0300

    Update TWITCH.py

--- scripts/TWITCH.py
+++ scripts/TWITCH.py
@@ -2,79 +2,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-
-# Configuring Chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run Chrome in headless mode (no GUI)
-chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration
-
-try:
-    # Initialize Chrome webdriver with the configured options
-    driver = webdriver.Chrome(options=chrome_options)
-
-    # URL of the Twitch search page
-    url_twitch = "https://www.twitch.tv/search?term=GRAN%20HERMANO"
-
-    # Open the desired URL
-    driver.get(url_twitch)
-
-    # Wait for the page to load (adjust the sleep time as needed)
-    time.sleep(5)
-
-    # Get page source after waiting
-    page_source = driver.page_source
-
-    # Parse the page source using BeautifulSoup
-    soup = BeautifulSoup(page_source, 'html.parser')
-
-    # Find all search result cards
-    cards = soup.find_all('div', class_='InjectLayout-sc-1i43xsx-0 fMQokC search-result-card')
-
-    # Open the file channel_info.txt in append mode
-    with open('channel_twitch.txt', 'w', encoding='utf-8') as file:
-        # Iterate through the found cards
-        for card in cards:
-            # Extrai o canal nome como já feito
-            channel_name = card.find('strong', class_='CoreText-sc-1txzju1-0 fMRfVf').text.strip()
-            
-            # Agora, extraímos os nomes de 'live_name' que estão nas tags <p> com o atributo especificado
-            live_names = card.find_all('p', class_='CoreText-sc-1txzju1-0 MveHm', attrs={'data-test-selector': 'search-result-live-channel__title'})
-            
-            # Extract group name (if available)
-            group_name = card.find('p', class_='CoreText-sc-1txzju1-0 exdYde').text.strip()
-            
-            # Extract logo image URL
-            logo_url = card.find('img', class_='search-result-card__img tw-image')['src']
-            
-            # Extract tvg-id
-            tvg_id = card.find('img', class_='search-result-card__img tw-image')['alt']
-            
-            # Format the output in the desired style
-            output_line = f"{channel_name} | Reality Show'S Live | {logo_url}"
-            
-            # Write to file
-            file.write(output_line + " | \n")
-            file.write(f"https://www.twitch.tv/{tvg_id}\n\n")   # Write Twitch URL in the next line
-
-except Exception as e:
-    print(f"Error: {e}")
-
-finally:
-    # Close the webdriver regardless of whether there was an exception or not
-    if 'driver' in locals():
-        driver.quit()
-
-#rato
-#!/usr/bin/python3
-
 import requests
 import os
-import sys
 import streamlink
 import logging
 from logging.handlers import RotatingFileHandler
 import json
 
+# Configurando logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -87,10 +22,12 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 
+# Banner do M3U
 banner = r'''
 #EXTM3U
 '''
 
+# Função para obter URL do stream usando Streamlink
 def grab(url):
     try:
         if url.endswith('.m3u') or url.endswith('.m3u8') or ".ts" in url:
@@ -109,7 +46,7 @@ def grab(url):
         logger.error("URL Error %s: %s", url, err)
         return None
 
-
+# Função para verificar URL
 def check_url(url):
     try:
         response = requests.head(url, timeout=15)
@@ -118,7 +55,7 @@ def check_url(url):
             return True
     except requests.exceptions.RequestException as err:
         pass
-    
+
     try:
         response = requests.head(url, timeout=15, verify=False)
         if response.status_code == 200:
@@ -127,80 +64,78 @@ def check_url(url):
     except requests.exceptions.RequestException as err:
         logger.error("URL Error %s: %s", url, err)
         return False
-    
+
     return False
 
-channel_data = []
-channel_data_json = []
+# Configuração do Selenium para coleta dos dados do Twitch
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 
-channel_info = os.path.abspath(os.path.join(os.path.dirname(__file__), '../channel_twitch.txt'))
+try:
+    # Inicializa o WebDriver
+    driver = webdriver.Chrome(options=chrome_options)
 
-with open(channel_info) as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('http:') and len(line.split("|")) == 4:
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
+    # URL da página de busca no Twitch
+    url_twitch = "https://www.twitch.tv/search?term=gran%20hermano"
+    driver.get(url_twitch)
+    time.sleep(5)
+
+    # Faz o parse do HTML da página
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    cards = soup.find_all('div', class_='InjectLayout-sc-1i43xsx-0 fMQokC search-result-card')
+
+    # Criação do arquivo .txt
+    channel_data = []
+    channel_info_path = 'channel_twitch.txt'
+
+    with open(channel_info_path, 'w', encoding='utf-8') as file:
+        for card in cards:
+            link_tag = card.find('a', class_='ScCoreLink-sc-16kq0mq-0')
+            title_tag = card.find('p', class_='CoreText-sc-1txzju1-0')
+            thumb_tag = card.find('img', class_='search-result-card__img')
+            group_tag = card.find('p', class_='CoreText-sc-1txzju1-0 exdYde')
+
+            if not link_tag or not title_tag or 'href' not in link_tag.attrs:
+                continue
+
+            # Extraindo o nome do canal, o link, a imagem e o título
+            tvg_id = link_tag['href'].strip('/')
+            channel_name = title_tag.text.strip()
+            thumb_url = thumb_tag['src'] if thumb_tag else ''
+            group_title = group_tag.text.strip() if group_tag else 'Unknown'
+
+            output_line = f"{channel_name} | {group_title} | Logo Not Found"
+            file.write(output_line + " | \n")
+            file.write(f"https://www.twitch.tv/{tvg_id}\n\n")
+
             channel_data.append({
                 'type': 'info',
-                'ch_name': ch_name,
-                'grp_title': grp_title,
-                'tvg_logo': tvg_logo,
-                'tvg_id': tvg_id
+                'ch_name': channel_name,
+                'tvg_id': tvg_id,
+                'url': f"https://www.twitch.tv/{tvg_id}",
+                'thumb': thumb_url,
+                'group_title': group_title
             })
-        else:
-            link = grab(line)
+
+    # Criação do arquivo .m3u
+    with open("TWITCH.m3u", "w", encoding="utf-8") as m3u_file:
+        m3u_file.write(banner)
+        prev_item = None
+    
+        for item in channel_data:
+            link = grab(item['url'])
             if link and check_url(link):
-                channel_data.append({
-                    'type': 'link',
-                    'url': link
-                })
+                m3u_file.write(f'\n#EXTINF:-1 group-title="RealityShow's Live" tvg-id="{item["tvg_id"]}", {item["ch_name"]}')
+                m3u_file.write('\n')
+                m3u_file.write(link)
+                m3u_file.write('\n')
 
-with open("TWITCH.m3u", "w") as f:
-    f.write(banner)
 
-    prev_item = None
+except Exception as e:
+    logger.error(f"Erro: {e}")
 
-    for item in channel_data:
-        if item['type'] == 'info':
-            prev_item = item
-        if item['type'] == 'link' and item['url']:
-            f.write(f'\n#EXTINF:-1 group-title="{prev_item["grp_title"]}" tvg-logo="{prev_item["tvg_logo"]}" tvg-id="{prev_item["tvg_id"]}",{prev_item["live_name"]}')
-            f.write('\n')
-            f.write(item['url'])
-            f.write('\n')
-
-prev_item = None
-
-for item in channel_data:
-    if item['type'] == 'info':
-        prev_item = item
-    if item['type'] == 'link' and item['url']:
-        channel_data_json.append( {
-            "id": prev_item["tvg_id"],
-            "name": prev_item["ch_name"],
-            "alt_names": [""],
-            "network": "",
-            "owners": [""],
-            "country": "AR",
-            "subdivision": "",
-            "city": "Buenos Aires",
-            "broadcast_area": [""],
-            "languages": ["spa"],
-            "categories": [prev_item["grp_title"]],
-            "is_nsfw": False,
-            "launched": "2016-07-28",
-            "closed": "2020-05-31",
-            "replaced_by": "",
-            "website": item['url'],
-            "logo": prev_item["tvg_logo"]
-        })
-
-with open("playlist.json", "w") as f:
-    json_data = json.dumps(channel_data_json, indent=2)
-    f.write(json_data)
+finally:
+    # Fecha o WebDriver
+    if 'driver' in locals():
+        driver.quit()
