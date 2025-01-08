1051c904805a044013e0bea5401bed449e512e0b
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-01-08 14:07:27 -0300

    Update TWITCH.py

--- scripts/TWITCH.py
+++ scripts/TWITCH.py
@@ -310,7 +310,7 @@ try:
             })
 
     # Gerar arquivo M3U com thumbnails
-    with open("TWITCH.m3u", "a", encoding="utf-8") as m3u_file:
+    with open("TWITCH.m3u", "w", encoding="utf-8") as m3u_file:
         m3u_file.write(banner)
         
         for item in channel_data:
