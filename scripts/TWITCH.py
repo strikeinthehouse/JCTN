eac52e496234738589c913cef862e80cc0c39bb5
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-01-09 18:25:38 -0300

    Update TWITCH.py

--- scripts/TWITCH.py
+++ scripts/TWITCH.py
@@ -120,7 +120,7 @@ try:
             link = grab(item['url'])
             if link and check_url(link):
                 m3u_file.write(
-                    f"\n#EXTINF:-1 tvg-logo=\"{item['thumb']}\" group-title=\"NEWS WORLD\",{item['ch_name']}"
+                    f"\n#EXTINF:-1 tvg-logo=\"{item['thumb']}\" group-title=\"MÉXICO\",{item['ch_name']}"
                 )
                 m3u_file.write('\n')
                 m3u_file.write(link)
