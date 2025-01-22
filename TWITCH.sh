c0453c96ca65f56c05f9a114de59601d67009d1e
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-01-22 03:50:21 -0300

    Update TWITCH.sh

--- TWITCH.sh
+++ TWITCH.sh
@@ -5,8 +5,6 @@ echo $(dirname $0)
 python3 -m pip install requests streamlink beautifulsoup4 selenium yt-dlp youtube-dl
 
 python3 $(dirname $0)/scripts/TWITCH.py
-python3 $(dirname $0)/scripts/TWITCH2.py
-python3 $(dirname $0)/scripts/TWITCH3.py
-python3 $(dirname $0)/scripts/TWITCH4.py
+
 
 echo Done!
