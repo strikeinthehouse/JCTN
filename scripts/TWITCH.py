d500529de47fde2a2bfeb4d1b453c9e3d75155c5
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-01-22 22:49:57 -0300

    Update TWITCH.py

--- scripts/TWITCH.py
+++ scripts/TWITCH.py
@@ -71,10 +71,10 @@ try:
 
     # URLs de tags fornecidas (incluindo as novas URLs)
     urls_twitch = [
-        "https://www.twitch.tv/directory/all/tags/BBB24HORAS",
         "https://www.twitch.tv/directory/all/tags/GrandeFratello",  # Nova tag
         "https://www.twitch.tv/directory/all/tags/bb18",  # Tag existente
         "https://www.twitch.tv/directory/all/tags/GranHermano",  # Nova tag
+        "https://www.twitch.tv/directory/all/tags/BBB24HORAS",
         "https://www.twitch.tv/directory/all/tags/granhermanoargentina",  # Nova tag
     ]
 
