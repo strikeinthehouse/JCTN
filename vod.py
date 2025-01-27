b96e69b59fe2b22429273382974ac128bec5a73f
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-01-27 01:00:19 -0300

    Update vod.py

--- vod.py
+++ vod.py
@@ -13,23 +13,7 @@ options.add_argument("--disable-infobars")
 
 # URLs dos vídeos Globoplay
 globoplay_urls = [
-    "https://globoplay.globo.com/v/2135579/",
-    "https://globoplay.globo.com/v/6120663/",
-    "https://globoplay.globo.com/v/2145544/",
-    "https://globoplay.globo.com/v/4039160/",
-    "https://globoplay.globo.com/v/4218681/",
-    "https://globoplay.globo.com/v/4064559/",
-    "https://globoplay.globo.com/v/2168377/",
-    "https://globoplay.globo.com/v/2923546/",
-    "https://globoplay.globo.com/v/3383021/",
-    "https://globoplay.globo.com/v/3065772/",
-    "https://globoplay.globo.com/v/6329086/",
-    "https://globoplay.globo.com/v/992055/",
-    "https://globoplay.globo.com/v/602497/",
-    "https://globoplay.globo.com/v/8713568/",
-    "https://globoplay.globo.com/v/8713568/",
-    "https://globoplay.globo.com/v/10747444/",
-    "https://globoplay.globo.com/v/10740500/",
+    "https://globoplay.globo.com/v/7832875/",
 ]
 
 # Função para extrair o link m3u8, título e thumbnail
