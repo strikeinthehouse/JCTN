37554cabdd8c0d7d8f76d649d4a39618cad9328c
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-02-04 04:55:05 -0300

    Update vod.py

--- vod.py
+++ vod.py
@@ -256,5 +256,5 @@ def search_google_images(query):
     return None
 
 # URL do arquivo M3U
-input_url = "https://github.com/punkstarbr/STR-YT/raw/refs/heads/main/LISTA%20ESTADOS%20UNIDOS.m3u"
+input_url = "https://github.com/strikeinthehouse/JCTN/raw/refs/heads/main/lista1.m3u"
 output_file = "lista1.m3u"
