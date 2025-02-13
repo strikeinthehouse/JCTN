e26f9602489a00bffe527ed2fbe6d89e19ba3dc2
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-02-13 19:11:06 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -25,6 +25,7 @@ globoplay_urls = [
     "https://g1.globo.com/pe/caruaru-regiao/video/transmissao-ao-vivo-do-abtv-5472979.ghtml",  # ABTV - Caruaru
     "https://globoplay.globo.com/v/5472979/",
     "https://globoplay.globo.com/v/2135579/",  # G1 RS - Telejornais da RBS TV
+    "https://globoplay.globo.com/ao-vivo/5472979/",
     "https://globoplay.globo.com/v/6120663/",  # G1 RS - Jornal da EPTV 1ª Edição - Ribeirão Preto
     "https://globoplay.globo.com/v/2145544/",  # G1 SC - Telejornais da NSC TV
     "https://globoplay.globo.com/v/4039160/",  # G1 CE - TV Verdes Mares ao vivo
