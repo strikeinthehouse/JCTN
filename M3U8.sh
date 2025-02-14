a6a246f475d10c42d79a4f42ea7bc251a8d35eda
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-02-13 23:19:01 -0300

    Create M3U8.sh

---
+++ M3U8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo $(dirname $0)
+
+python3 -m pip install requests streamlink beautifulsoup4 selenium yt-dlp youtube-dl
+
+python3 $(dirname $0)/GLOBO.py
+python3 $(dirname $0)/it.py
+
+
+echo Done!
