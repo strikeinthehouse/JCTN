3069c2efc80ecfe015305e7c49b88ca7a57cea1b
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-02-17 18:09:46 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -17,7 +17,7 @@ options.add_argument("--disable-infobars")
 driver = webdriver.Chrome(options=options)
 
 # URL base para pesquisa
-base_url = "https://duckduckgo.com/?q=g1+vivo+site%3Aglobo.com&t=h_&iar=videos&start=1&iax=videos&ia=videos"
+base_url = "https://duckduckgo.com/?q=assista+ao+site%3Aglobo.com&t=h_&iar=videos&start=1&iax=videos&ia=videos"
 
 try:
     driver.get(base_url)
