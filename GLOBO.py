91e8f12201fe785572058de898380a0723c4c33b
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-02-18 19:28:16 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -14,36 +14,9 @@ options.add_argument("--disable-infobars")
 
 # URLs dos vídeos Globoplay
 globoplay_urls = [
-    "https://g1.globo.com/rr/roraima/video/ao-vivo-assista-o-jornal-de-roraima-1a-edicao-2923545-1739458038240.ghtml",
-    "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/bom-dia-cidade-ribeirao-preto.ghtml",  # Bom Dia Cidade Ribeirão Preto
-    "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv1.ghtml",  # EPTV 1ª Edição - Ribeirão Preto
-    "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv-2-ribeirao-e-franca-ao-vivo.ghtml",  # EPTV 2ª Edição - Ribeirão e Franca
-    "https://g1.globo.com/pe/petrolina-regiao/ao-vivo/ao-vivo-assista-ao-gr2.ghtml",  # GR2 - Petrolina
-    "https://g1.globo.com/ap/ao-vivo/assista-ao-bdap-desta-sexta-feira-7.ghtml",  # BDAP - Amapá
-    "https://globoplay.globo.com/v/1467373/",  # Globoplay - Transmissão ao vivo
-    "https://globoplay.globo.com/v/4064559/",  # G1 ao vivo - Transmissão ao vivo
-    "https://globoplay.globo.com/v/2135579/",  # G1 RS - Telejornais da RBS TV
+    "https://kcnawatch.org/korea-central-tv-livestream/",
+    "https://kcnawatch.org/kctv-archive/67b4910973ea9",  # EPTV 1ª Edição - Ribeirão Preto
     "https://globoplay.globo.com/ao-vivo/5472979/",
-    "https://globoplay.globo.com/v/5472979/",
-    "https://globoplay.globo.com/v/6120663/",  # G1 RS - Jornal da EPTV 1ª Edição - Ribeirão Preto
-    "https://globoplay.globo.com/v/2145544/",  # G1 SC - Telejornais da NSC TV
-    "https://globoplay.globo.com/v/4039160/",  # G1 CE - TV Verdes Mares ao vivo
-    "https://globoplay.globo.com/v/6329086/",  # Globo Esporte BA - Travessia Itaparica-Salvador ao vivo
-    "https://globoplay.globo.com/v/11999480/",  # G1 ES - Jornal Regional ao vivo
-    "https://g1.globo.com/al/alagoas/ao-vivo/assista-aos-telejornais-da-tv-gazeta-de-alagoas.ghtml",  # Telejornais da TV Gazeta de Alagoas
-    "https://globoplay.globo.com/ao-vivo/3667427/",  # Globoplay - Transmissão ao vivo
-    "https://globoplay.globo.com/v/4218681/",  # G1 Triângulo Mineiro - Transmissão ao vivo
-    "https://globoplay.globo.com/v/12945385/",  # Globoplay - Transmissão ao vivo
-    "https://globoplay.globo.com/v/3065772/",  # G1 MS - Transmissão ao vivo em MS
-    "https://globoplay.globo.com/v/2923579/",  # G1 AP - Telejornais da Rede Amazônica
-    "https://g1.globo.com/am/amazonas/ao-vivo/assista-aos-telejornais-da-rede-amazonica.ghtml",  # Telejornais da Rede Amazônica - Amazonas
-    "https://globoplay.globo.com/v/2923546/",  # G1 AC - Jornais da Rede Amazônica
-    "https://globoplay.globo.com/v/2168377/",  # Telejornais da TV Liberal
-    "https://globoplay.globo.com/v/992055/",  # G1 ao vivo - Transmissão ao vivo
-    "https://globoplay.globo.com/v/602497/",  # ge.globo - Transmissão ao vivo
-    "https://globoplay.globo.com/v/8713568/",  # Globo Esporte RS - Gauchão ao vivo
-    "https://globoplay.globo.com/v/10747444/",  # CBN SP - Transmissão ao vivo
-    "https://globoplay.globo.com/v/10740500/",  # CBN RJ - Transmissão ao vivo
 ]
 
 def extract_globoplay_data(url):
