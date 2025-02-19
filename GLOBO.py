e5f78eca42250d01dbdbb30bc702757d163cd7a2
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-02-19 00:45:09 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -27,7 +27,7 @@ globoplay_urls = [
     "https://g1.globo.com/pe/petrolina-regiao/ao-vivo/ao-vivo-assista-ao-gr2.ghtml",  # GR2 - Petrolina
     "https://g1.globo.com/ap/ao-vivo/assista-ao-bdap-desta-sexta-feira-7.ghtml",  # BDAP - Amapá
     "https://globoplay.globo.com/v/1467373/",  # Globoplay - Transmissão ao vivo
-    "https://globoplay.globo.com/v/4064559/",  # G1 ao vivo - Transmissão ao vivo
+    "https://globoplay.globo.com/v/4064559/",  # G1 SERVIÇO
     "https://globoplay.globo.com/v/992055/",  # G1 SERVIÇO
     "https://globoplay.globo.com/v/602497/",  # ge SERVIÇO
     "https://globoplay.globo.com/v/2135579/",  # G1 RS - Telejornais da RBS TV
