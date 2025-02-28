d05d264e840356a8ee461479b56d2e9fcaf9a88a
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-02-28 02:30:18 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -14,7 +14,6 @@ options.add_argument("--disable-infobars")
 
 # URLs dos vídeos Globoplay
 globoplay_urls = [
-    "https://globoads.globo.com/sobre-nos/agenda-de-negocios/sxsw-2025.ghtml",
     "https://g1.globo.com/rr/roraima/video/ao-vivo-assista-o-jornal-de-roraima-1a-edicao-2923545-1739458038240.ghtml",
     "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/bom-dia-cidade-ribeirao-preto.ghtml",  # Bom Dia Cidade Ribeirão Preto
     "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv1.ghtml",  # EPTV 1ª Edição - Ribeirão Preto
