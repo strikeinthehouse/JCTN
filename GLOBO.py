b45d4d6136103df5f159b0148fedf60cd2320a0f
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-03-03 08:59:53 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
+import concurrent.futures
 
 # Configurações do Chrome
 options = Options()
@@ -12,10 +13,7 @@ options.add_argument("--window-size=1280,720")
 options.add_argument("--disable-infobars")
 
 # Caminho do driver do Chrome
-driver_path = '/path/to/chromedriver'
-
-# Inicializa o driver
-driver = webdriver.Chrome(executable_path=driver_path, options=options)
+driver = webdriver.Chrome(options=options)
 
 # Acessa o site
 driver.get("https://www.google.com/search?q=vivo+site%3Aglobo.com&sca_esv=35aba76f9e0fd39c&udm=7&biw=1912&bih=954&ei=OZjFZ9rXDObb1sQP7rSP4Ak&ved=0ahUKEwia2Pvv7O2LAxXmrZUCHW7aA5wQ4dUDCBE&uact=5&oq=vivo+site%3Aglobo.com&gs_lp=EhZnd3Mtd2l6LW1vZGVsZXNzLXZpZGVvIhN2aXZvIHNpdGU6Z2xvYm8uY29tSM0CUABY0AFwAHgBkAEAmAFtoAHXAaoBAzAuMrgBA8gBAPgBAZgCAKACAJgDAJIHAKAHlgE&sclient=gws-wiz-modeless-video#ip=1")
