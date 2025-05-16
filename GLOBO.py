2b65aae545dec2b50d58fc6479b5bfe255c764cc
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-05-16 19:46:30 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -128,6 +128,8 @@ globoplay_urls = [
     "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv-2-ribeirao-e-franca-ao-vivo.ghtml",  # EPTV 2ª Edição - Ribeirão e Franca
     "https://g1.globo.com/pe/petrolina-regiao/ao-vivo/ao-vivo-assista-ao-gr2.ghtml",  # GR2 - Petrolina
     "https://g1.globo.com/ap/ao-vivo/assista-ao-bdap-desta-sexta-feira-7.ghtml",  # BDAP - Amapá
+    "https://g1.globo.com/ap/ao-vivo/assista-ao-jap2-deste-sabado-10.ghtml",
+    "https://g1.globo.com/pr/parana/ao-vivo/acontece-agora-em-curitiba.ghtml",
     "https://globoplay.globo.com/v/1467373/",  # Globoplay - Transmissão ao vivo
     "https://globoplay.globo.com/v/1328766/",  # G1 SERVIÇO
     "https://globoplay.globo.com/v/4064559/",  # G1 SERVIÇO
